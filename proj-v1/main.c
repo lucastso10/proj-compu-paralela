31421247b7e9e4d4285da3a2f1971f3417ee0cca
Lucas Teixeira Soares <84486266+lucastso10@users.noreply.github.com>
2023-05-02 13:46:49 -0400

    proj serial funcionando

--- proj-v1/main.c
+++ proj-v1/main.c
@@ -4,34 +4,40 @@
 mpf_t n, e, temp, c;
 mpz_t calc;
 
-void fat(int quant){
-  mpz_mul_ui(calc, calc, quant+1);
-}
-
 int main(void) {
-  int i;
+        int i;
+        char* output;
+        mp_exp_t exp;
 
-  mpf_set_default_prec(10000);
-  mpf_init(e);
-  mpf_init(n);
-  mpf_init(temp);
-  mpf_init(c);
-  mpf_set_d(e, 1.0);
-  mpf_set_d(c, 1.0);
-  mpz_init_set_ui(calc, 1);
- 
-  for(i = 0; i < 1000; i++){
-    fat(i);
-    mpf_set_z(n, calc);
-    mpf_div(temp, c, n);
-    mpf_add(e, e, temp);
-  }
+        // inicializa os floats
+        mpf_set_default_prec(100000);
+        mpf_init(e);
+        mpf_init(n);
+        mpf_init(temp);
+        mpf_init(c);
+        mpf_set_d(e, 1.0);
+        mpf_set_d(c, 1.0);
+        mpz_init_set_ui(calc, 1);
 
-  printf ("a!  =  ");
-  mpf_out_str(NULL, 10, 10000, e);
-  mpf_clear(n);
-  mpf_clear(e);
-  printf("\n");
-  
-  return 0;
+        // loop para calcular o valor de e.
+        for(i = 0; i < 100; i++){
+                mpz_mul_ui(calc, calc, i+1); // calcula o fatorial para esse loop
+                mpf_set_z(n, calc); // copia int (mpz) para um float (mpf) 
+                mpf_div(temp, c, n); // divide c (1) por n (fatorial) e guarda em temp
+                mpf_add(e, temp, e); // soma o temp para e, o resultado
+        }
+
+        // printa o float
+        printf ("e = ");
+        output = mpf_get_str(NULL, &exp, 10, 0, e); // magia para printar o float
+        printf("%.*s.%s\n", (int)exp, output, output+exp);
+
+        // libera a memoria
+        mpf_clear(n);
+        mpf_clear(e);
+        mpf_clear(temp);
+        mpf_clear(c);
+        mpz_clear(calc);
+
+        return 0;
 }
