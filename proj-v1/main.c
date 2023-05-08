c733d74880352454e3569ea82fef38d25fbdd6f6
Ubuntu <ubuntu@ip-172-31-15-68.ec2.internal>
2023-05-08 12:44:25 +0000

    tirei o uso de int em proj-v1

--- proj-v1/main.c
+++ proj-v1/main.c
@@ -5,7 +5,6 @@
 
 int main(int argc, char* argv[]) {
 	mpf_t n, e, temp, c;
-	mpz_t calc;
 
         int i, pres;
         char* output;
@@ -16,31 +15,28 @@ int main(int argc, char* argv[]) {
 
         // inicializa os floats
         mpf_set_default_prec(100000);
-        mpf_init(n);
+        // mpf_inits(n, temp, NULL);
         mpf_init(temp);
+	mpf_init_set_d(n, 1.0);
         mpf_init_set_d(e, 1.0);
         mpf_init_set_d(c, 1.0);
-        mpz_init_set_ui(calc, 1);
 
         // loop para calcular o valor de e.
         for(i = 0; i < pres; i++){
-                mpz_mul_ui(calc, calc, i+1); // calcula o fatorial para esse loop
-                mpf_set_z(n, calc); // copia int (mpz) para um float (mpf) 
+                mpf_mul_ui(n, n, i+1); // calcula o fatorial para esse loop
                 mpf_div(temp, c, n); // divide c (1) por n (fatorial) e guarda em temp
                 mpf_add(e, temp, e); // soma o temp para e, o resultado
         }
 
         // printa o float
-        printf ("e = ");
         output = mpf_get_str(NULL, &exp, 10, 0, e); // converte float para string
-        printf("%.*s.%s\n", (int)exp, output, output+exp); // printa a string com o valor do float
+        printf("e = %.*s.%s\n", (int)exp, output, output+exp); // printa a string com o valor do float
 
         // libera a memoria
         mpf_clear(n);
         mpf_clear(e);
         mpf_clear(temp);
         mpf_clear(c);
-        mpz_clear(calc);
 
         return 0;
 }
