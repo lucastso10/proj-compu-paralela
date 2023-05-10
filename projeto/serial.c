3ef9be645ebfb70e7b3344ac65921aa045931552
Ubuntu <ubuntu@ip-172-31-15-68.ec2.internal>
2023-05-10 13:15:06 +0000

    novo jeito de calcular euler

--- projeto/serial.c
+++ projeto/serial.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-	mpf_t n, e, temp;
-
+	mpf_t n, e;
+	
         int precisao, casas = 30000, i;
 
 	precisao = atoi(argv[1]);
@@ -13,16 +13,25 @@ int main(int argc, char* argv[]) {
         mpf_set_default_prec(100000);
 
         // inicializa os floats
-        mpf_init(temp);
 	mpf_init_set_d(n, 1.0);
         mpf_init_set_d(e, 1.0);
 
         // loop para calcular o valor de e.
+	
+	mpf_div_ui(n, n, precisao);
+	mpf_add_ui(n, n, 1);
+
+	for(i = 0; i < precisao; i++){
+		mpf_mul(e, e, n);
+	}
+
+	/*
         for(i = 0; i < precisao; i++){
                 mpf_mul_ui(n, n, i+1); // calcula o fatorial para esse loop
                 mpf_ui_div(temp, 1, n); // divide 1 por n (fatorial) e guarda em temp
                 mpf_add(e, temp, e); // soma o temp para e, o resultado
         }
+	*/
 
         // printa o float
 	gmp_printf("e = %.*Ff \n", casas, e);
@@ -30,7 +39,6 @@ int main(int argc, char* argv[]) {
         // libera a memoria
         mpf_clear(n);
         mpf_clear(e);
-        mpf_clear(temp);
 
         return 0;
 }
