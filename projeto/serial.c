946d019bef2faf8618b422b6806aa9daf3a3ad81
Ubuntu <ubuntu@ip-172-31-15-68.ec2.internal>
2023-05-10 11:46:43 +0000

    Revert "adicionei speedup"
    This reverts commit 222ae7f85d0c689b53f1722f18f8df3370b53104.

--- projeto/serial.c
+++ projeto/serial.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
+#include <gmp.h>
 #include <stdlib.h>
-#include <mpfr.h>
 
 int main(int argc, char* argv[]) {
-	mpfr_t n, e, temp;
+	mpf_t n, e, temp;
 
         int precisao, casas = 30000, i;
 
 	precisao = atoi(argv[1]);
 	
 	// define o tamanho máximo dos floats
-        mpfr_set_default_prec(100000);
+        mpf_set_default_prec(100000);
 
         // inicializa os floats
-        mpfr_init(temp);
-	mpfr_init_set_d(n, 1.0, MPFR_RNDN);
-        mpfr_init_set_d(e, 1.0, MPFR_RNDN);
+        mpf_init(temp);
+	mpf_init_set_d(n, 1.0);
+        mpf_init_set_d(e, 1.0);
 
         // loop para calcular o valor de e.
         for(i = 0; i < precisao; i++){
-                mpfr_mul_ui(n, n, i+1, MPFR_RNDF); // calcula o fatorial para esse loop
-                mpfr_ui_div(temp, 1, n, MPFR_RNDF); // divide 1 por n (fatorial) e guarda em temp
-                mpfr_add(e, temp, e, MPFR_RNDF); // soma o temp para e, o resultado
+                mpf_mul_ui(n, n, i+1); // calcula o fatorial para esse loop
+                mpf_ui_div(temp, 1, n); // divide 1 por n (fatorial) e guarda em temp
+                mpf_add(e, temp, e); // soma o temp para e, o resultado
         }
 
         // printa o float
-	mpfr_out_str(stdout, 10, 30000, e, MPFR_RNDF);
+	gmp_printf("e = %.*Ff \n", casas, e);
 
         // libera a memoria
-        mpfr_clear(n);
-        mpfr_clear(e);
-        mpfr_clear(temp);
+        mpf_clear(n);
+        mpf_clear(e);
+        mpf_clear(temp);
 
         return 0;
 }
